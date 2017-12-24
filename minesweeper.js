291156d0588814c1061edb4a952fe0d36eb91939
Ashraf Nazar <ashraf.caspian@gmail.com>
2017-12-24 13:33:56 +0000

    Dynamically generate a minesweeper game

--- minesweeper.js
+++ minesweeper.js
@@ -1,17 +1,47 @@
+const generatePlayerBoard = (numberOfRows, numberOfColumns) => {
+  const board = [];
+  for(let rowIndex = 0; rowIndex < numberOfRows; rowIndex++) {
+    const row = [];
+    for(let columnIndex = 0; columnIndex < numberOfColumns; columnIndex++) {
+      row.push(' ');
+    }
+    board.push(row);
+  }
+  return board;
+};
+
+const generateBombBoard = (numberOfRows, numberOfColumns, numberOfBombs) => {
+  const board = [];
+  for(let rowIndex = 0; rowIndex < numberOfRows; rowIndex++) {
+    const row = [];
+    for(let columnIndex = 0; columnIndex < numberOfColumns; columnIndex++) {
+      row.push(null);
+    }
+    board.push(row);
+  }
+  let numberOfBombsPlaced = 0;
+  while (numberOfBombsPlaced < numberOfBombs) {
+    const randomRowIndex = (Math.floor(Math.random() * (numberOfRows)));
+    const randomColumnIndex = (Math.floor(Math.random() * (numberOfColumns)));
+    board[randomRowIndex][randomColumnIndex] = 'B' ;
+    numberOfBombsPlaced++;
+    // code in the while loop may place bombs on top of already existing bombs.
+  };
+  return board
+};
+
 const printBoard = board => {
-  console.log('Current Board:')
-  console.log(board[0].join(' | '));
-  console.log(board[1].join(' | '));
-  console.log(board[2].join(' | '));
-}
+  console.log(board.map(row => 
+    row.join(' | ')).join('\n'));
+};
 
-let board = [
-[' ', ' ', ' '], 
-[' ', ' ', ' '], 
-[' ', ' ', ' '],
-];
+let playerBoard = generatePlayerBoard(3, 4);
+let bombBoard = generateBombBoard(3, 4, 5);
 
-console.log(printBoard(board));
-board[0][1] = '1';
-board[2][2] = 'B';
-console.log(printBoard(board));
+console.log('Player Board: ');
+printBoard(playerBoard);
+console.log('Bomb Board: ');
+printBoard(bombBoard);
+
+
+
